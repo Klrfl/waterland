4ea4d32596d07df5ad438a281a35f13c9394e2b4
Efraim Munthe <efraimmunthe101@gmail.com>
2023-02-04 10:30:34 +0700

    remove overlay from JS

--- js/script.js
+++ js/script.js
@@ -2,27 +2,17 @@ const openNavbar = document.getElementById("open-navbar");
 const closeNavbar = document.getElementById("close-navbar");
 const navbar = document.getElementById("nav-links-container");
 
-let overlay = document.createElement("div");
-overlay.setAttribute("id", "overlay");
-
 openNavbar.addEventListener("click", () => {
   navbar.classList.toggle("active");
-  if (document.body !== overlay.parentElement) {
-    document.body.append(overlay);
-  } else {
-    overlay.remove();
-  }
 });
 
 closeNavbar.addEventListener("click", () => {
   navbar.classList.remove("active");
-  overlay.remove();
 });
 
 // when overlay gets clicked, close navbar
 overlay.addEventListener("click", () => {
   navbar.classList.remove("active");
-  overlay.remove();
 });
 
 // highlight nav on scroll
